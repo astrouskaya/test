23762b73fe383b346049bccb7d7a6d208d0d8528
astrouskaya <85347059+astrouskaya@users.noreply.github.com>
2021-06-08 15:21:53 +0300

    added: solution of the task 3

---
+++ ConditionalBranchingJavaScript/Task 3 (Show number sign).js
@@ -0,0 +1,10 @@
+if (OS === 'Linux') {
+    console.log ("You've got the Linux!");
+} else if (OS === 'MacOS'
+    || OS === 'Windows XP'
+    || OS === 'Windows 7'
+    || OS === 'Windows 8') {
+    console.log ( 'Okay we support these OS too' );
+} else {
+    console.log ( 'We hope that this OS is ok!' );
+}
