70f2180081f727eadab4d678227451a5fa88a1d7
Maryna <m.astrouskaya@andersenlab.com>
2021-06-15 17:30:19 +0300

    complited tasks

---
+++ FunctionsJS/Task1.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<meta charset="utf-8">
+<html lang="en-us">
+<body>
+<script>
+    function checkAge(age) {
+        return (age > 18) ? true : confirm('Родители разрешили?');
+    }
+</script>
+</body>
+</html>
---
+++ FunctionsJS/Task3.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<meta charset="utf-8">
+<html lang="en-us">
+<body>
+<script>
+    function min(a, b) {
+        if (a < b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+</script>
+</body>
+</html>
