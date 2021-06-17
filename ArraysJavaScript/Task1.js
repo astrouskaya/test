40f33f587543ebff54760ee4c196bb03bcc92aa0
Maryna <m.astrouskaya@andersenlab.com>
2021-06-17 15:02:50 +0300

    ArraysJaTasks

---
+++ ArraysJavaScript/Task1.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<meta charset="utf-8">
+<html lang="en-us">
+<body>
+<script>
+let styles = ['Джаз', 'Блюз',];
+styles.push('Рок-н-ролл');
+console.log(styles);
+
+
+// let classicMusic.splice(1, 0, 'Классика');
+// console.log(styles);
+</script>
+</body>
+</html>
